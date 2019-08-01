5653c43327731a5a094c65b65d722fa6ac700661
dinglili <dinglili@xylink.com>
2019-08-01 19:21:45 +0800

    session

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const querystring = require('querystring');
 const handleBlogRouter = require('./src/router/blog');
 const handleUserRouter = require('./src/router/user');
 
+const SESSION_DATA = {}
+
+// 设置cookies过期时间
+const getCookiesExpires = () => {
+  const d = new Date()
+  d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+  return d.toGMTString()
+}
+
 const getPostData = (req) => {
   const promise = new Promise((resolve, reject) => {
     if (req.method !== 'POST') {
@@ -43,9 +52,23 @@ const serverHandle = (req, res) => {
   const cookieStr = req.headers.cookie || ''
   cookieStr.split(';').forEach(val => {
     const key = val.split('=')[0].trim()
-    const value = val.split('=')[1].trim()
+    const value = val.split('=')[1]
     req.cookie[key] = value
   })
+
+  // 解析session
+  let needSetCookie = false
+  let userId = req.cookie.userid;
+  if (userId) {
+    if (!SESSION_DATA[userId]) {
+      SESSION_DATA[userId] = {}
+    }
+  } else {
+    needSetCookie = true
+    userId = `${Date.now()}_${Math.random()}`
+    SESSION_DATA[userId] = {}
+  }
+  req.session = SESSION_DATA[userId]
 
   getPostData(req).then(postData => {
     req.body = postData;
@@ -54,6 +77,10 @@ const serverHandle = (req, res) => {
     const blogResult = handleBlogRouter(req, res)
     if (blogResult) {
       blogResult.then(blogData => {
+        if (needSetCookie) {
+          // 服务端设置cookie
+          res.setHeader('Set-Cookie', `userid=${userId}; path=/; httpOnly; expires=${getCookiesExpires()}`)
+        }
         res.end(JSON.stringify(blogData));
         return;
       });
@@ -64,6 +91,10 @@ const serverHandle = (req, res) => {
     const userResult = handleUserRouter(req, res)
     if (userResult) {
       userResult.then(userData => {
+        if (needSetCookie) {
+          // 服务端设置cookie
+          res.setHeader('Set-Cookie', `userid=${userId}; path=/; httpOnly; expires=${getCookiesExpires()}`)
+        }
         res.end(
           JSON.stringify(userData)
         );
